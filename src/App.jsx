fc4ab33a9d991af0f1ced6aa6e9e28ba1f184c34
simmeringstew <dev.95a2e@simplelogin.com>
2023-09-06 14:10:19 -0400

    Added buttons for selecting mode of play

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import './App.css'
+import { useState } from 'react';
+import Card from './Card';
+import './styles/app.css';
 
 const App = () => {
   return (
-    <></>
+    <div className='container'>
+      <Card />
+      <div className='button-group'>
+        <button className='easy'>Easy</button>
+        <button className='medium'>Medium</button>
+        <button className='hard'>Hard</button>
+        <button className='advanced'>Advanced</button>
+        <button className='phrases'>Phrases</button>
+        <button className='reset'>Reset</button>
+      </div>
+    </div>
   );
 }
 
