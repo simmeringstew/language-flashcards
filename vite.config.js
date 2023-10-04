316d2767bbe99f046207bf74c477c1ccc606cd3f
simmeringstew <dev.95a2e@simplelogin.com>
2023-10-04 13:34:10 -0400

    Added a . to folder

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/language-flashcards',
+  base: './language-flashcards',
   plugins: [react()],
 })
