36d0853f29f5cf1d29733c587a382ecce23b60b8
simmeringstew <dev.95a2e@simplelogin.com>
2023-10-04 13:32:17 -0400

    Added gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pages/dist',
+  base: '/language-flashcards',
   plugins: [react()],
 })
