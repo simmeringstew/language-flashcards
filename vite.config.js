4b7609559657b0fa34616817c594a3b35242aa95
simmeringstew <dev.95a2e@simplelogin.com>
2023-10-05 11:59:18 -0400

    Removed base

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://simmeringstew.github.io/language-flashcards/',
-  plugins: [react()],
-  // build: {
-  //   assetsDir: '.'
-  // }
+  plugins: [react()]
 })
