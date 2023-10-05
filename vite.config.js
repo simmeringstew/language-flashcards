19ab1e5d02059cb299128d74aa0790d2f1b607c3
simmeringstew <dev.95a2e@simplelogin.com>
2023-10-05 11:47:17 -0400

    Added a build and assets

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/language-flashcards/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    assetsDir: '.'
+  }
 })
