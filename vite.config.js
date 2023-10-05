2b915ff86cfad477fc94bf54a35cfb2c9b617f28
simmeringstew <dev.95a2e@simplelogin.com>
2023-10-05 11:53:29 -0400

    Changed base to github url

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/language-flashcards/',
+  base: 'https://simmeringstew.github.io/language-flashcards/',
   plugins: [react()],
-  build: {
-    assetsDir: '.'
-  }
+  // build: {
+  //   assetsDir: '.'
+  // }
 })
